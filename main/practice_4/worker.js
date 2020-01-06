6909a40c03e2485cb8eb4d41f81250df738d413b
JinHui <jh731448520@163.com>
2020-01-06 22:55:03 +0800

    [JinHui]{feat}-{#5.2}:finish 4.practice_4

--- main/practice_4/worker.js
+++ main/practice_4/worker.js
@@ -1,1 +1,13 @@
 
+import Person from "../../main/practice_4/person.js";
+
+class Worker extends Person {
+  constructor(name,age) {
+    super(name,age);
+  }
+  introduce() {
+    return super.introduce() + " I am a Worker. I have a job.";
+  }
+}
+
+export default Worker;
