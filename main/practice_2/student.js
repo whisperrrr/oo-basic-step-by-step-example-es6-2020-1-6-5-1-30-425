00bdb53fa07acb883ecf0f2a47e40f5d5210b150
JinHui <jh731448520@163.com>
2020-01-06 22:41:06 +0800

    [JinHui]{feat}-{#5.2}:finish 2.practice_2 and 3.practice_3

--- main/practice_2/student.js
+++ main/practice_2/student.js
@@ -1,1 +1,13 @@
+import Person from "../../main/practice_2/person.js";
 
+class Student extends Person {
+  constructor(name,age,klass) {
+    super(name,age);
+    this.klass = 2;
+  }
+  introduce() {
+    return "I am a Student. I am at Class " + this.klass + ".";
+  }
+}
+
+export default Student;
--- main/practice_3/worker.js
+++ main/practice_3/worker.js
@@ -1,1 +1,12 @@
+import Person from "../../main/practice_2/person.js";
 
+class Worker extends Person {
+  constructor(name,age) {
+    super(name,age);
+  }
+  introduce() {
+    return "I am a Worker. I have a job.";
+  }
+}
+
+export default Worker;
