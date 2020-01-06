475584a9bfa6079549cc450de4766e4734c03f92
JinHui <jh731448520@163.com>
2020-01-07 00:14:18 +0800

    [JinHui]{feat}-{#5.2}:finish 6.practice_6

--- main/practice_6/student.js
+++ main/practice_6/student.js
@@ -1,1 +1,14 @@
+import Person from "../../main/practice_6/person.js";
+
+class Student extends Person {
+  constructor(name,age,klass) {
+    super(name,age);
+    this.klass = 2;
+  }
+  introduce() {
+    return super.introduce() + " I am a Student. I am at Class " + this.klass + ".";
+  }
+}
+
+export default Student;
 
--- main/practice_6/teacher.js
+++ main/practice_6/teacher.js
@@ -1,1 +1,19 @@
+import Person from "../../main/practice_6/person.js";
+
+class Teacher extends Person {
+  constructor(name,age,klass) {
+    super(name,age);
+    this.klass = klass;
+  }
+  introduce() {
+    if (this.klass) {
+      return super.introduce() + " I am a Teacher. I teach Class " + this.klass + ".";
+    }
+    else {
+      return super.introduce() + " I am a Teacher. "+ "I teach No Class.";
+    }   
+  }
+}
+
+export default Teacher;
 
