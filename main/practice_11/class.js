8baa6fc832c6e4206d6da24131962eee69b86790
JinHui <jh731448520@163.com>
2020-01-08 20:38:59 +0800

    [JinHui]{feat}-{#5.2}:finish basic 11.practice_11

--- main/practice_11/class.js
+++ main/practice_11/class.js
@@ -1,1 +1,39 @@
-
+import Student from "../../main/practice_11/student.js";
+class Class {
+  constructor(number,leader) {
+    this.number = number;
+  }
+  getDisplayName() {
+    return "Class " + this.number;
+  }
+  assignLeader(student) {
+    if (student.klass.number === this.number) {
+      this.leader = student;
+      if (this.teacher) {
+        console.log("I am " + this.teacher.name + " I know " + student.name + " become Leader of Class " + this.number + ".");
+      }
+    }
+    else {
+      console.log("It is not one of us.");
+    }
+  }
+  appendMember(student) {
+    student.klass = this;
+    if (this.teacher) {
+      console.log("I am " + this.teacher.name + " I know " + student.name + " has joined Class " + this.number + ".");
+    }
+  }
+  isIn(student) {
+    if (student.klass === this) {
+      return true;
+    }
+    return false;
+  }
+  registerAssignLeaderListener(teacher) {
+    return this.teacher = teacher;
+  }
+  registerJoinListener(teacher) {
+    return this.teacher = teacher;
+  }
+}
+export default Class;
--- main/practice_11/teacher.js
+++ main/practice_11/teacher.js
@@ -1,1 +1,42 @@
+import Person from "../../main/practice_11/person.js";
+import Class from "../../main/practice_11/class.js";
 
+class Teacher extends Person {
+  constructor(id,name,age,klasses) {
+    super(id,name,age);
+    this.klasses = klasses;
+    if (klasses) {
+      this.klasses.length = klasses.length;
+    }
+  }
+  introduce() {
+    if (this.klasses) {
+      return super.introduce() + " I am a Teacher. I teach Class " + getClassNumber(this.klasses) + ".";
+    }
+    else {
+      return super.introduce() + " I am a Teacher. "+ "I teach No Class.";
+    }   
+  }
+  isTeaching(student) {
+    for (let klass of this.klasses) {
+      if (klass.isIn(student)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  listen() {
+    console.log("in");
+  }
+}
+
+function getClassNumber(klasses) {
+  let klassNum = [];
+  for (let klass of klasses) {
+    klassNum.push(klass.number);
+  }
+  klassNum = klassNum.join(", ");
+  return klassNum;
+}
+
+export default Teacher;
