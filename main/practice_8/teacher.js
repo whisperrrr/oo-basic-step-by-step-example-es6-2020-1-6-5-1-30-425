9805aebb40f89976a8ffec5e4c63342562f79363
JinHui <jh731448520@163.com>
2020-01-08 17:31:47 +0800

    [JinHui]{feat}-{#5.2}:finish 8.practice_8

--- main/practice_8/teacher.js
+++ main/practice_8/teacher.js
@@ -1,1 +1,18 @@
+import Person from "../../main/practice_8/person.js";
+import Class from "../../main/practice_8/class.js";
 
+class Teacher extends Person {
+  constructor(id,name,age,klass) {
+    super(id,name,age);
+    this.klass = klass;
+  }
+  introduce() {
+    if (this.klass) {
+      return super.introduce() + " I am a Teacher. I teach " + this.klass.getDisplayName() + ".";
+    }
+    else {
+      return super.introduce() + " I am a Teacher. "+ "I teach No Class.";
+    }   
+  }
+}
+export default Teacher;
