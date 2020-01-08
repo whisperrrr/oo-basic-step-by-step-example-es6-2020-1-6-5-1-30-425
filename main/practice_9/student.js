0f20965a3c6d6a00fa2d9c0ff58c8ca813d6eea2
JinHui <jh731448520@163.com>
2020-01-08 17:51:44 +0800

    [JinHui]{feat}-{#5.2}:finish 9.practice_9

--- main/practice_9/student.js
+++ main/practice_9/student.js
@@ -1,1 +1,18 @@
+import Person from "../../main/practice_9/person.js";
+import Class from "../../main/practice_9/class.js";
 
+class Student extends Person {
+  constructor(id,name,age,klass) {
+    super(id,name,age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    if (this.klass.leader === this) {
+      return super.introduce() + " I am a Student. I am Leader of " + this.klass.getDisplayName() + ".";
+    }
+    return super.introduce() + " I am a Student. I am at " + this.klass.getDisplayName() + ".";
+  }
+}
+
+export default Student;
