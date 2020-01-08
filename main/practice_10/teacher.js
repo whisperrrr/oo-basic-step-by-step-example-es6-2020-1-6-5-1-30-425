aed6194c59e71a9acacd379246dfd8832e756d8f
JinHui <jh731448520@163.com>
2020-01-08 19:30:58 +0800

    [JinHui]{feat}-{#5.2}:finish 10.practice_10

--- main/practice_10/teacher.js
+++ main/practice_10/teacher.js
@@ -1,1 +1,31 @@
+import Person from "../../main/practice_10/person.js";
+import Class from "../../main/practice_10/class.js";
 
+class Teacher extends Person {
+  constructor(id,name,age,klasses) {
+    super(id,name,age);
+    this.klasses = klasses;
+    if (klasses) {
+      this.klasses.length = klasses.length;
+    }
+  }
+  introduce() {
+    if (this.klasses) {
+      return super.introduce() + " I am a Teacher. I teach Class " + getClassNumber(this.klasses) + ".";
+    }
+    else {
+      return super.introduce() + " I am a Teacher. "+ "I teach No Class.";
+    }   
+  }
+}
+
+function getClassNumber(klasses) {
+  let klassNum = [];
+  for (let klass of klasses) {
+    klassNum.push(klass.number);
+  }
+  klassNum = klassNum.join(", ");
+  return klassNum;
+}
+
+export default Teacher;
